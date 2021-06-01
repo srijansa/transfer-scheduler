2601adbfb9680042a10f50f5f831f1002912c6a8
Jacob Goldverg <jgoldverg@gmail.com>
2021-06-01 10:30:09 -0400

    forgot to handle null instead of less than 1 in createTransferOptions

--- rabbitmq-scheduler/src/main/java/com/rabbitMq/rabbitmqscheduler/DTO/TransferOptions.java
+++ rabbitmq-scheduler/src/main/java/com/rabbitMq/rabbitmqscheduler/DTO/TransferOptions.java
@@ -23,9 +23,9 @@ public class TransferOptions {
     public static TransferOptions createTransferOptionsFromUser(UserTransferOptions userTransferOptions) {
         return TransferOptions.builder()
                 .compress(userTransferOptions.getCompress() != null && userTransferOptions.getCompress())
-                .concurrencyThreadCount(userTransferOptions.getConcurrencyThreadCount() < 1? 1: userTransferOptions.getConcurrencyThreadCount())
-                .pipeSize(userTransferOptions.getPipeSize() < 1?1: userTransferOptions.getPipeSize())
-                .parallelThreadCount(userTransferOptions.getParallelThreadCount() < 1? 1 : userTransferOptions.getParallelThreadCount() )
+                .concurrencyThreadCount(userTransferOptions.getConcurrencyThreadCount() == null? 1: userTransferOptions.getConcurrencyThreadCount())
+                .pipeSize(userTransferOptions.getPipeSize() == null ?1: userTransferOptions.getPipeSize())
+                .parallelThreadCount(userTransferOptions.getParallelThreadCount() == null? 0 : userTransferOptions.getParallelThreadCount() )
                 .optimizer(userTransferOptions.getOptimizer() == null ? "" : userTransferOptions.getOptimizer())
                 .encrypt(userTransferOptions.getEncrypt() != null && userTransferOptions.getEncrypt())
                 .overwrite(userTransferOptions.isOverwrite())
