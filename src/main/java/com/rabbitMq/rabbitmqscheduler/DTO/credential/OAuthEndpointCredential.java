9263431d45ca76d9605a0ee1d5eea0ad5507c5f8
Jacob Goldverg <jgoldverg@gmail.com>
2021-08-16 13:09:18 -0400

    hopefully fixes the Date json serialization bug

--- src/main/java/com/rabbitMq/rabbitmqscheduler/DTO/credential/OAuthEndpointCredential.java
+++ src/main/java/com/rabbitMq/rabbitmqscheduler/DTO/credential/OAuthEndpointCredential.java
@@ -1,6 +1,9 @@
 package com.rabbitMq.rabbitmqscheduler.DTO.credential;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.util.Date;
@@ -9,9 +12,12 @@ import java.util.Date;
  * POJO for storing OAuth Credentials
  */
 @Data
+@NoArgsConstructor
+@Setter
 public class OAuthEndpointCredential extends EndpointCredential {
     private String token;
     private boolean tokenExpires = false;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Date expiresAt;
     private String refreshToken;
     private boolean refreshTokenExpires = false;
