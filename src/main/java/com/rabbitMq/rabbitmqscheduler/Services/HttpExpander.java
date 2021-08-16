e53a7ae90d1071958de6355de4389bf8bd97ec86
Jacob Goldverg <jgoldverg@gmail.com>
2021-08-16 12:19:29 -0400

    forgot this

--- src/main/java/com/rabbitMq/rabbitmqscheduler/Services/HttpExpander.java
+++ src/main/java/com/rabbitMq/rabbitmqscheduler/Services/HttpExpander.java
@@ -1,2 +1,20 @@
-package com.rabbitMq.rabbitmqscheduler.Services;public class HttpExpander {
+package com.rabbitMq.rabbitmqscheduler.Services;
+
+import com.rabbitMq.rabbitmqscheduler.DTO.EntityInfo;
+import com.rabbitMq.rabbitmqscheduler.DTO.credential.EndpointCredential;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class HttpExpander implements FileExpander{
+    @Override
+    public void createClient(EndpointCredential credential) {
+
+    }
+
+    @Override
+    public List<EntityInfo> expandedFileSystem(List<EntityInfo> userSelectedResources, String basePath) {
+        return null;
+    }
 }
