e23b33c2558bc1b772c4bd8d6b5380766893b0cc
Jacob Goldverg <jgoldverg@gmail.com>
2023-02-14 14:22:46 -0500

    Transfer scheduler now pushes updates to the TransferNode

--- src/main/java/com/rabbitMq/rabbitmqscheduler/Controller/ApplyTransferParamsController.java
+++ src/main/java/com/rabbitMq/rabbitmqscheduler/Controller/ApplyTransferParamsController.java
@@ -2,8 +2,8 @@ package com.rabbitMq.rabbitmqscheduler.Controller;
 
 import com.rabbitMq.rabbitmqscheduler.DTO.TransferParams;
 import com.rabbitMq.rabbitmqscheduler.Sender.MessageSender;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,12 +12,13 @@ public class ApplyTransferParamsController {
 
     MessageSender messageSender;
 
-    public ApplyTransferParamsController(MessageSender messageSender){
+    public ApplyTransferParamsController(MessageSender messageSender) {
         this.messageSender = messageSender;
     }
 
     @PutMapping("/apply/application/params")
-    public void consumeApplicationParamChange( @RequestBody TransferParams transferParams){
+    public ResponseEntity<String> consumeApplicationParamChange(@RequestBody TransferParams transferParams) {
         this.messageSender.sendApplicationParams(transferParams, transferParams.getTransferNodeName());
+        return ResponseEntity.ok().build();
     }
 }
