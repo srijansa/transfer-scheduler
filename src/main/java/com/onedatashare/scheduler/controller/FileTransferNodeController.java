5e6fb823b9040995f0cfef66f36fed29ca0edc6f
Jacob Goldverg <jgoldverg@gmail.com>
2024-11-02 10:21:48 -0400

    removed the extra controller for ods connectors

--- src/main/java/com/onedatashare/scheduler/controller/FileTransferNodeController.java
+++ src/main/java/com/onedatashare/scheduler/controller/FileTransferNodeController.java
@@ -2,6 +2,7 @@ package com.onedatashare.scheduler.controller;
 
 import com.onedatashare.scheduler.model.FileTransferNodeMetaData;
 import com.onedatashare.scheduler.services.FileTransferNodeDiscovery;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,17 @@ public class FileTransferNodeController {
     }
 
     @GetMapping("/{user}")
-    public List<FileTransferNodeMetaData> getUserOdsConnectors(@PathVariable("user") String user) {
-        return this.fileTransferNodeDiscovery.getUsersFileTransferNodes(user);
+    public ResponseEntity<List<FileTransferNodeMetaData>> getUserOdsConnectors(@PathVariable("user") String user) {
+        return ResponseEntity.ok(this.fileTransferNodeDiscovery.getUsersFileTransferNodes(user));
     }
 
     @GetMapping("/ods")
-    public List<FileTransferNodeMetaData> getOdsNodes() {
-        return this.fileTransferNodeDiscovery.getOdsNodes();
+    public ResponseEntity<List<FileTransferNodeMetaData>> getOdsNodes() {
+        return ResponseEntity.ok(this.fileTransferNodeDiscovery.getOdsNodes());
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Integer> totalConnectedFileTransferNodes(){
+        return ResponseEntity.ok(this.fileTransferNodeDiscovery.totalConnectedFileTransferNodes());
     }
 }
